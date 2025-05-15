bb6b67d7c798d21e759d233ba1d4987f44a1be75
Calvin W <calvinwill0102@gmail.com>
2025-05-15 15:31:56 +0700

    mailjs_update

--- views/auth/mail.js
+++ views/auth/mail.js
@@ -1,26 +1,17 @@
+
 // Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
 // Firebase config
 const firebaseConfig = {
-
   apiKey: "AIzaSyCdzaCYqLgUGhsSKUsG2bVHIgeLq7AFWOQ",
-
   authDomain: "oxfordform2.firebaseapp.com",
-
-  databaseURL: "https://oxfordform2-default-rtdb.firebaseio.com",
-
   projectId: "oxfordform2",
-
-  storageBucket: "oxfordform2.firebasestorage.app",
-
+  storageBucket: "oxfordform2.appspot.com",
   messagingSenderId: "570492980550",
-
   appId: "1:570492980550:web:dfbc14f380cd7fcf4d6947",
-
   measurementId: "G-P3JLGDW0SN"
-
 };
 
 // Initialize Firebase and Firestore
@@ -42,6 +33,7 @@ document.getElementById("submitBtn").addEventListener("click", async (e) => {
     const combinedData = {};
 
     // Collect and merge all section data
+    
     // sectionKeys.forEach((key) => {
     //   const sectionData = JSON.parse(localStorage.getItem(key)) || {};
     //   Object.assign(combinedData, sectionData);
@@ -58,8 +50,6 @@ document.getElementById("submitBtn").addEventListener("click", async (e) => {
       }
     });
     
-
-    data.student_id = user.uid;
 
     // Upload to Firestore
     await addDoc(collection(db, "OxfordForm"), combinedData);
